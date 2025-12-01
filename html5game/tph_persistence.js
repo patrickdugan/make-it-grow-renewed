a4b0618c271d59912d072b039aeed6a75973c924
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-12-01 18:27:07 -0300

    game

---
+++ html5game/tph_persistence.js
@@ -0,0 +1,33 @@
+function gd_load()
+{
+	return load();
+}
+
+function gd_save(data,hypertime)
+{
+	return save(data,hypertime);
+}
+
+function gd_get_user_status()
+{
+	return isLoggedIn();
+}
+
+function gd_log(msg)
+{
+  console.log(msg);
+  return 1;
+}
+
+function gd_get_tutorial_step (step) {
+  return tutorial_step(step);
+}
+
+function gd_save_tutorial_status(status) {
+  return save_tutorial(status);
+}
+
+function gd_mixpanel_register(name, params) {
+  mixpanel.track(name); //,JSON.parse(params));
+  return 1;
+}
